d3139764e2ba54c9f7238d12cfbac6316955b7ce
marcosh1212 <core.editor=vim-tiny>
2023-03-13 15:35:47 -0300

    incluso faixa VPC nos outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "vpc_id" {
   value = aws_vpc.vpc-teste.id
 }
 
+output "VPC_CIDR" {
+  value = aws_vpc.vpc-teste.cidr_block
+}
+
 output "subnet_id" {
   value = aws_subnet.private_subnet.id
 }
