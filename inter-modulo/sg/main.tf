9da258f21ed24fab0d533d42e5bdbf80009be813
marcosh1212 <core.editor=vim-tiny>
2023-03-22 23:00:11 -0300

    corrigido sg

--- inter-modulo/sg/main.tf
+++ inter-modulo/sg/main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "sg-teste" {
     protocol  = "tcp"
     from_port = 22
     to_port   = 22
-        cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -23,8 +23,17 @@ resource "aws_security_group" "sg-teste" {
     protocol  = "icmp"
     from_port = -1
     to_port   = -1
-        cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    description = "All traffic out"
+    protocol  = "-1"
+    from_port = 0
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   
   tags = {
     Name = "sg-teste"
